dfe6ee353232d9304e6c95a85ed91955f65e48c5
harrylbi <122418589+harrylbi@users.noreply.github.com>
2025-04-13 19:47:48 +0700

    Update Palindrome.java

--- palindrome/Palindrome.java
+++ palindrome/Palindrome.java
@@ -1,30 +1,21 @@
+package palindrome;
+
 import java.util.Scanner;
 
 public class Palindrome {
 
     public static void main(String[] args) {
-        // Membuat scanner untuk input
+        TextGame game = new TextGame();
         Scanner scanner = new Scanner(System.in);
 
         // Meminta input dari pengguna
         System.out.print("Masukkan kata: ");
         String input = scanner.nextLine();
 
-        // Memanggil metode untuk mengecek palindrome
-        if (isPalindrome(input)) {
-            System.out.println("Hasil: Palindrome");
-        } else {
-            System.out.println("Hasil: Bukan Palindrome");
-        }
+        // Menampilkan hasil
+        game.displayResult(input);
 
         // Menutup scanner
         scanner.close();
-    }
-
-    // Metode untuk mengecek apakah sebuah string palindrome
-    public static boolean isPalindrome(String text) {
-        String cleanedText = text.replaceAll("\\s+", "").toLowerCase(); // Menghilangkan spasi dan mengubah ke huruf kecil
-        String reversedText = new StringBuilder(cleanedText).reverse().toString();
-        return cleanedText.equals(reversedText);
     }
 }
