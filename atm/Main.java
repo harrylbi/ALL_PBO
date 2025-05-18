b66efbda4181a926f0f024e13ad1b53ef345cd5c
harrylbi <harrylbi014@gmail.com>
2025-05-19 00:05:23 +0700

    nambah option di main

--- atm/Main.java
+++ atm/Main.java
@@ -24,33 +24,84 @@ public class Main {
         //     pilihan = scanner.nextInt();
         // }
         do {
+
+
             System.out.println("Selemat datang di ATM BANK LBI");
-                            
-            System.out.println("1.Information Saldo");
-            System.out.println("2.Transfer");
-            System.out.println("3.Tarik tunai");
-            System.out.println("4.Setor tunai");
-            System.out.println("5.keluar");
-            
-            Nasabah daftarNasabah;
+            String inputnama = scanner.next();
+            Nasabah namaNasabah = null;
+
+            for (Nasabah n : daftarNasabah){
+                if (n.getNasabah().equalsIgnoreCase(inputnama)){
+                    namaNasabah = n;
+                    break;
+
+                }
+            }
+
+            if (namaNasabah == null ){
+                System.out.println("nama nasabah salah periksa kembali namamu");
+                continue;
+            }
+
+            System.out.println("\nMenu:");
+            System.out.println("1. Informasi Saldo");
+            System.out.println("2. Transfer");
+            System.out.println("3. Tarik Tunai");
+            System.out.println("4. Setor Tunai");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih menu (1-5): ");
+            pilihan = scanner.nextInt();
+
             switch (pilihan) {
                 case 1:
-                    System.out.println("Saldo" + daftarNasabah.getSaldo());
+                    System.out.println("Saldo" + namaNasabah.getSaldo());
                     break;
                 case 2:
+                    System.out.println("masukan nama tujuan");
+                    String tujuaNama = scanner.next();
+                    Nasabah tujuan = null;
+                    for (Nasabah n : daftarNasabah){
+                        if (n.getNasabah().equalsIgnoreCase(tujuaNama)){
+                            tujuan = n;
+                            break;
+                        }
+                    }
+                    if(tujuan == null){
+                        System.out.println("Nasabah tujuan tidak ditemukan");
+                        break;
+                    }
+                    System.out.println("masukan jumlah transfer: ");
+                    double jumlahTransfer = scanner.nextDouble();
+                    if (namaNasabah.getSaldo() >= jumlahTransfer){
+                        namaNasabah.tarik(jumlahTransfer);
+                        tujuan.setor(jumlahTransfer);
+                        System.out.println("Transfer berhasil");
+
+                    } else {
+                        System.out.println("saldo tidak mencukupi");
+                    }
                     break;
                 case 3:
+                    System.out.println("Masukan jumlah yang ditarik");
+                    double tarik = scanner.nextDouble();
+                    namaNasabah.tarik(tarik);
                     break;
                 case 4:
+                    System.out.println("Masukab jumlah setor");
+                    double setor = scanner.nextDouble();
+                    namaNasabah.setor(setor);
                     break;
                 case 5:
+                    System.out.println("terimakasih sudah menggunakan ATM LBi");
                     break;
             
                 default:
+                    System.out.println("pilihan tidak valid");
                     break;
             }    
 
-    } while (pilihan !=5);
 
-           
+       } while (true);
+
+    }           
 }
